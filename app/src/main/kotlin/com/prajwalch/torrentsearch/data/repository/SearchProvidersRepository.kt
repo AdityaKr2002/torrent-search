AdityaKr2002/torrent-search

commit b8957d6d1be8cecdc6bbfe478ef1f0410299a742

refactor(SearchProvidersRepository): Better naming for functions + improve implementations
Signed-off-by: prajwalch <prajwal.chapagain58@gmail.com>

app/src/main/kotlin/com/prajwalch/torrentsearch/data/repository/SearchProvidersRepository.kt
@@ -37,10 +37,8 @@ import com.prajwalch.torrentsearch.providers.XXXTracker
 import com.prajwalch.torrentsearch.providers.Yts
 
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.firstOrNull
-import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.toList
 
 import java.util.UUID
 
@@ -78,67 +76,48 @@ class SearchProvidersRepository @Inject constructor(
         Yts(),
     )
 
-    // TODO: Remove this or handle enabled by default search providers properly.
-    fun getEnabledSearchProvidersId(): Set<SearchProviderId> {
+    suspend fun getSearchProvidersByCategory(category: Category): List<SearchProvider> {
+        val searchProviders = getSearchProviders().toList().flatten()
+
+        return if (category == Category.All) {
+            searchProviders
+        } else {
+            searchProviders.filterByCategory(category)
+        }
+    }
+
+    fun getSearchProviders(): Flow<List<SearchProvider>> {
+        return torznabConfigDao.getAllConfigs()
+            .map { configEntities ->
+                configEntities.map {
+                    TorznabSearchProvider(id = it.searchProviderId, config = it.toDomain())
+                }
+            }
+            .map { torznabProvidersInstance -> builtins + torznabProvidersInstance }
+    }
+
+    private fun List<SearchProvider>.filterByCategory(category: Category) = this.filter {
+        val specializedCategory = it.info.specializedCategory
+        (specializedCategory == Category.All) || (category == specializedCategory)
+    }
+
+    fun getDefaultSearchProvidersId(): Set<SearchProviderId> {
         return builtins.filter { it.info.enabledByDefault }.map { it.info.id }.toSet()
     }
 
-    fun observeSearchProvidersInfo(): Flow<List<SearchProviderInfo>> {
-        val builtinSearchProvidersInfoFlow = flowOf(builtins.map { it.info })
-        val torznabSearchProvidersInfoFlow = torznabConfigDao.getAllConfigs().map {
-            it.toSearchProviderInfo()
-        }
+    fun getSearchProvidersInfo(): Flow<List<SearchProviderInfo>> {
+        val builtinProvidersInfo = builtins.map { it.info }
 
-        return combine(
-            builtinSearchProvidersInfoFlow,
-            torznabSearchProvidersInfoFlow
-        ) { builtinInfos, torznabInfos ->
-            builtinInfos + torznabInfos
-        }.map { infos ->
-            infos.sortedBy { it.name }
-        }
+        return torznabConfigDao.getAllConfigs()
+            .map { configEntities -> configEntities.toSearchProviderInfo() }
+            .map { torznabProvidersInfo -> builtinProvidersInfo + torznabProvidersInfo }
+            // TODO: Remove this. Sorting is not the concern of repository.
+            .map { searchProviderInfos -> searchProviderInfos.sortedBy { it.name } }
     }
 
-    fun observeSearchProvidersCount(): Flow<Int> {
-        return torznabConfigDao.getConfigsCount().map { it + builtins.size }
-    }
-
-    suspend fun getSearchProvidersInstance(category: Category): List<SearchProvider> {
-        val searchProviders = getSearchProvidersInstance()
-
-        if (category == Category.All) {
-            return searchProviders
-        }
-
-        return searchProviders.filter {
-            // NOTE: Currently, if the search provider's specialized is set to
-            //       `All` we can't surely know whether the underlying server
-            //       supports setting specific category or not.
-            //
-            //       For example: TorrentCSV does allow to search any type of
-            //       torrent but it doesn't support setting specific category
-            //       explicitly. Meaning, we can't ask it to search torrents
-            //       of specific category.
-            //
-            //       To address this issue, force each and every search provider
-            //       to list out all categories they allow or support to set
-            //       explicitly instead of single `specializedCategory`.
-            (it.info.specializedCategory == Category.All) || (category == it.info.specializedCategory)
-        }
-    }
-
-    suspend fun getSearchProvidersInstance(): List<SearchProvider> {
-        val builtinSearchProvidersFlow = flowOf(builtins)
-        val torznabSearchProvidersFlow = torznabConfigDao.getAllConfigs().map { entities ->
-            entities.map { TorznabSearchProvider(id = it.searchProviderId, config = it.toDomain()) }
-        }
-
-        return combine(
-            builtinSearchProvidersFlow,
-            torznabSearchProvidersFlow,
-        ) { builtins, externals ->
-            builtins + externals
-        }.firstOrNull().orEmpty()
+    fun getSearchProvidersCount(): Flow<Int> {
+        return torznabConfigDao.getConfigsCount()
+            .map { torznabProvidersCount -> builtins.size + torznabProvidersCount }
     }
 
     suspend fun checkTorznabConnection(url: String, apiKey: String): TorznabConnectionCheckResult {

app/src/main/kotlin/com/prajwalch/torrentsearch/data/repository/SettingsRepository.kt
@@ -45,7 +45,7 @@ class SettingsRepository @Inject constructor(
     val enabledSearchProvidersId: Flow<Set<SearchProviderId>> = dataStore
         .getOrDefault(
             key = ENABLED_SEARCH_PROVIDERS_ID,
-            default = searchProvidersRepository.getEnabledSearchProvidersId(),
+            default = searchProvidersRepository.getDefaultSearchProvidersId(),
         )
 
     val defaultCategory: Flow<Category> = dataStore

app/src/main/kotlin/com/prajwalch/torrentsearch/domain/SearchTorrentsUseCase.kt
@@ -61,11 +61,11 @@ class SearchTorrentsUseCase @Inject constructor(
     }
 
     private suspend fun getEnabledSearchProviders(category: Category): List<SearchProvider> {
-        val searchProviders = searchProvidersRepository
-            .getSearchProvidersInstance(category = category)
         val enabledSearchProvidersId = settingsRepository.enabledSearchProvidersId.first()
 
-        return searchProviders.filter { it.info.id in enabledSearchProvidersId }
+        return searchProvidersRepository
+            .getSearchProvidersByCategory(category = category)
+            .filter { it.info.id in enabledSearchProvidersId }
     }
 
     private suspend fun getSearchResultsLimit(): MaxNumResults {

app/src/main/kotlin/com/prajwalch/torrentsearch/ui/settings/SettingsViewModel.kt
@@ -96,7 +96,7 @@ class SettingsViewModel @Inject constructor(
 
         val searchProvidersStatFlow = combine(
             settingsRepository.enabledSearchProvidersId.map { it.size },
-            searchProvidersRepository.observeSearchProvidersCount(),
+            searchProvidersRepository.getSearchProvidersCount(),
             SearchSettingsUiState::SearchProvidersStat,
         )
         val searchSettingsFlow = combine(
@@ -160,7 +160,7 @@ class SettingsViewModel @Inject constructor(
     /** Disables NSFW and Unsafe search providers which are currently enabled. */
     private suspend fun disableRestrictedSearchProviders() {
         val enabledSearchProvidersInfo = combine(
-            searchProvidersRepository.observeSearchProvidersInfo(),
+            searchProvidersRepository.getSearchProvidersInfo(),
             settingsRepository.enabledSearchProvidersId,
         ) { searchProvidersInfo, enabledSearchProvidersId ->
             searchProvidersInfo.filter { it.id in enabledSearchProvidersId }

app/src/main/kotlin/com/prajwalch/torrentsearch/ui/settings/searchproviders/SearchProvidersViewModel.kt
@@ -50,7 +50,7 @@ class SearchProvidersViewModel @Inject constructor(
 
     val uiState = combine(
         selectedCategory,
-        searchProvidersRepository.observeSearchProvidersInfo(),
+        searchProvidersRepository.getSearchProvidersInfo(),
         settingsRepository.enabledSearchProvidersId,
         ::createUiState,
     ).stateIn(
@@ -108,7 +108,7 @@ class SearchProvidersViewModel @Inject constructor(
     fun enableAllSearchProviders() {
         viewModelScope.launch {
             val allSearchProvidersId = searchProvidersRepository
-                .observeSearchProvidersInfo()
+                .getSearchProvidersInfo()
                 .map { infos -> infos.map { it.id } }
                 .firstOrNull()
                 ?: return@launch
@@ -132,7 +132,7 @@ class SearchProvidersViewModel @Inject constructor(
     fun resetEnabledSearchProvidersToDefault() {
         viewModelScope.launch {
             settingsRepository.setEnabledSearchProvidersId(
-                providersId = searchProvidersRepository.getEnabledSearchProvidersId(),
+                providersId = searchProvidersRepository.getDefaultSearchProvidersId(),
             )
         }
     }

commit e26204b063a90095d9b62bb8c00d3fae734426a4

fix(SearchProvidersRepository): Regression introduced in b8957d6
Signed-off-by: prajwalch <prajwal.chapagain58@gmail.com>

app/src/main/kotlin/com/prajwalch/torrentsearch/data/repository/SearchProvidersRepository.kt
@@ -35,13 +35,10 @@ import com.prajwalch.torrentsearch.providers.UIndex
 import com.prajwalch.torrentsearch.providers.XXXClub
 import com.prajwalch.torrentsearch.providers.XXXTracker
 import com.prajwalch.torrentsearch.providers.Yts
-
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.toList
-
 import java.util.UUID
-
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -77,7 +74,7 @@ class SearchProvidersRepository @Inject constructor(
     )
 
     suspend fun getSearchProvidersByCategory(category: Category): List<SearchProvider> {
-        val searchProviders = getSearchProviders().toList().flatten()
+        val searchProviders = getSearchProviders().firstOrNull() ?: return emptyList()
 
         return if (category == Category.All) {
             searchProviders