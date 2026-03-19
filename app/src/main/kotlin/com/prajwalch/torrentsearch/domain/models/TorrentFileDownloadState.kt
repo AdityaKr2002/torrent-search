b02cd5a6986f5ec4aab4816a8f8cebe7b6ae3e23
prajwalch <prajwal.chapagain58@gmail.com>
2026-03-19 12:03:58 +0545

    refactor: Remove `TorrentFileDownloadState` model from the domain package
    Signed-off-by: prajwalch <prajwal.chapagain58@gmail.com>

--- app/src/main/kotlin/com/prajwalch/torrentsearch/domain/models/TorrentFileDownloadState.kt
+++
@@ -1,9 +0,0 @@
-package com.prajwalch.torrentsearch.domain.models
-
-sealed interface TorrentFileDownloadState {
-    data object Empty : TorrentFileDownloadState
-    
-    data object Downloading : TorrentFileDownloadState
-
-    data class Success(val fileName: String) : TorrentFileDownloadState
-}
