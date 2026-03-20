9f0b4ba464382072da05a9dad6def5d29bfb3863
prajwalch <prajwal.chapagain58@gmail.com>
2026-03-20 19:10:54 +0545

    refactor(searchhistory): Move `DeleteAllConfirmationDialog` to its own component package
    Signed-off-by: prajwalch <prajwal.chapagain58@gmail.com>

--- app/src/main/kotlin/com/prajwalch/torrentsearch/ui/searchhistory/SearchHistoryScreen.kt
+++ app/src/main/kotlin/com/prajwalch/torrentsearch/ui/searchhistory/SearchHistoryScreen.kt
@@ -3,14 +3,11 @@ package com.prajwalch.torrentsearch.ui.searchhistory
 import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
@@ -23,7 +20,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalClipboard
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -33,6 +29,7 @@ import com.prajwalch.torrentsearch.extension.copyText
 import com.prajwalch.torrentsearch.ui.component.ArrowBackIconButton
 import com.prajwalch.torrentsearch.ui.component.DeleteForeverIconButton
 import com.prajwalch.torrentsearch.ui.component.EmptyPlaceholder
+import com.prajwalch.torrentsearch.ui.searchhistory.component.DeleteAllConfirmationDialog
 import com.prajwalch.torrentsearch.ui.searchhistory.component.SearchHistoryList
 
 import kotlinx.coroutines.launch
@@ -112,38 +109,4 @@ fun SearchHistoryScreen(
             )
         }
     }
-}
-
-@Composable
-private fun DeleteAllConfirmationDialog(
-    onDismiss: () -> Unit,
-    onConfirm: () -> Unit,
-    modifier: Modifier = Modifier,
-) {
-    AlertDialog(
-        modifier = modifier,
-        onDismissRequest = onDismiss,
-        icon = {
-            Icon(
-                painter = painterResource(R.drawable.ic_delete_forever),
-                contentDescription = null,
-            )
-        },
-        title = {
-            Text(text = stringResource(R.string.search_history_dialog_title_clear_history))
-        },
-        text = {
-            Text(text = stringResource(R.string.search_history_dialog_message_clear_history))
-        },
-        confirmButton = {
-            TextButton(onClick = onConfirm) {
-                Text(text = stringResource(R.string.search_history_button_clear))
-            }
-        },
-        dismissButton = {
-            TextButton(onClick = onDismiss) {
-                Text(text = stringResource(R.string.button_cancel))
-            }
-        },
-    )
 }
