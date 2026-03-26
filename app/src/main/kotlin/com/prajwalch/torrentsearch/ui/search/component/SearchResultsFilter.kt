0cc52adb5a932b2ebe60c6aedeb80aec12f09e6d
prajwalch <prajwal.chapagain58@gmail.com>
2026-03-26 16:43:30 +0545

    refactor(search): Move providers filter bottom sheet to its own package
    Signed-off-by: prajwalch <prajwal.chapagain58@gmail.com>

--- app/src/main/kotlin/com/prajwalch/torrentsearch/ui/search/component/SearchResultsFilter.kt
+++ app/src/main/kotlin/com/prajwalch/torrentsearch/ui/search/component/SearchResultsFilter.kt
@@ -2,28 +2,15 @@ package com.prajwalch.torrentsearch.ui.search.component
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material3.DropdownMenuItem
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilterChip
 import androidx.compose.material3.FilterChipDefaults
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ModalBottomSheet
-import androidx.compose.material3.PlainTooltip
 import androidx.compose.material3.Text
-import androidx.compose.material3.TooltipAnchorPosition
-import androidx.compose.material3.TooltipBox
-import androidx.compose.material3.TooltipDefaults
-import androidx.compose.material3.rememberTooltipState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -31,7 +18,6 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 
@@ -40,10 +26,7 @@ import com.prajwalch.torrentsearch.domain.model.Category
 import com.prajwalch.torrentsearch.ui.categoryStringResource
 import com.prajwalch.torrentsearch.ui.component.RoundedDropdownMenu
 import com.prajwalch.torrentsearch.ui.search.FilterOptions
-import com.prajwalch.torrentsearch.ui.search.SearchProviderFilterOption
 import com.prajwalch.torrentsearch.ui.theme.spaces
-
-import kotlinx.collections.immutable.ImmutableList
 
 @Composable
 fun SearchResultsFilter(
@@ -138,114 +121,6 @@ fun SearchResultsFilter(
                     onCategorySelect = onUpdateCategory,
                 )
             }
-        }
-    }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-private fun SearchProvidersFilterBottomSheet(
-    onDismiss: () -> Unit,
-    filterOptions: ImmutableList<SearchProviderFilterOption>,
-    onToggleSearchProvider: (providerName: String) -> Unit,
-    onSelectAll: () -> Unit,
-    onDeselectAll: () -> Unit,
-    onInvertSelection: () -> Unit,
-    modifier: Modifier = Modifier,
-) {
-    ModalBottomSheet(modifier = modifier, onDismissRequest = onDismiss) {
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = MaterialTheme.spaces.large),
-        ) {
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically,
-            ) {
-                Text(
-                    modifier = Modifier.weight(1f),
-                    text = stringResource(R.string.search_providers_filter_bottom_sheet_title),
-                    style = MaterialTheme.typography.titleMedium,
-                )
-
-                Row(verticalAlignment = Alignment.CenterVertically) {
-                    SearchProvidersFilterActionButton(
-                        onClick = onSelectAll,
-                        painter = painterResource(R.drawable.ic_select_all),
-                        contentDescription = stringResource(
-                            R.string.search_providers_filter_action_select_all,
-                        ),
-                    )
-                    SearchProvidersFilterActionButton(
-                        onClick = onDeselectAll,
-                        painter = painterResource(R.drawable.ic_deselect_all),
-                        contentDescription = stringResource(
-                            R.string.search_providers_filter_action_deselect_all,
-                        ),
-                    )
-                    SearchProvidersFilterActionButton(
-                        onClick = onInvertSelection,
-                        painter = painterResource(R.drawable.ic_flip),
-                        contentDescription = stringResource(
-                            R.string.search_providers_filter_action_invert_selection,
-                        ),
-                    )
-                }
-            }
-
-            SearchProvidersChipRow(
-                modifier = Modifier.padding(vertical = MaterialTheme.spaces.large),
-                filterOptions = filterOptions,
-                onToggleSearchProvider = onToggleSearchProvider,
-            )
-        }
-    }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-private fun SearchProvidersFilterActionButton(
-    onClick: () -> Unit,
-    painter: Painter,
-    contentDescription: String,
-    modifier: Modifier = Modifier,
-) {
-    val tooltipPositionProvider =
-        TooltipDefaults.rememberTooltipPositionProvider(TooltipAnchorPosition.Above)
-
-    TooltipBox(
-        modifier = modifier,
-        positionProvider = tooltipPositionProvider,
-        tooltip = { PlainTooltip { Text(text = contentDescription) } },
-        state = rememberTooltipState(),
-    ) {
-        IconButton(onClick = onClick) {
-            Icon(
-                painter = painter,
-                contentDescription = contentDescription,
-            )
-        }
-    }
-}
-
-@Composable
-private fun SearchProvidersChipRow(
-    filterOptions: ImmutableList<SearchProviderFilterOption>,
-    onToggleSearchProvider: (String) -> Unit,
-    modifier: Modifier = Modifier,
-) {
-    FlowRow(
-        modifier = modifier,
-        horizontalArrangement = Arrangement.spacedBy(MaterialTheme.spaces.small),
-        itemVerticalAlignment = Alignment.CenterVertically,
-    ) {
-        filterOptions.forEach {
-            FilterChip(
-                selected = it.selected,
-                onClick = { onToggleSearchProvider(it.searchProviderName) },
-                label = { Text(text = it.searchProviderName) },
-            )
         }
     }
 }
